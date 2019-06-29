ffc7b9ca5aaf2f2d1636a5b9ad7a1a326694916c
laoliya <936366286@qq.com>
2019-06-30 03:18:55 +0800

    add

--- src/components/dyw/common/subset/upload.vue
+++ src/components/dyw/common/subset/upload.vue
@@ -1,25 +1,36 @@
 <template>
-    <div class="d1">
-        <el-upload
-            class="upload-demo"
-            action="https://jsonplaceholder.typicode.com/posts/"
-            :on-preview="handlePreview"
-            :on-remove="handleRemove"
-            :file-list="fileList"
-            list-type="picture">
-            <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-        </el-upload>
-    </div>
+  <div class="d1">
+    <el-upload
+  class="upload-demo"
+  ref="upload"
+  action="http://localhost:3000/up/ceshi"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :file-list="fileList"
+  :auto-upload="false"
+  list-type="picture"
+  >
+  <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+  <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+</el-upload>
+        </div>
     </template>
+
 <script>
 export default {
     data() {
       return {
-        fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+        fileList: [],
+        a:'',
       };
     },
     methods: {
+  
+      submitUpload() {
+        console.log(this.fileList);
+        this.$refs.upload.submit(this.result);
+      },
       handleRemove(file, fileList) {
         console.log(file, fileList);
       },
